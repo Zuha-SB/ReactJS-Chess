b6eae8031d1530d139da8678dcacbe08701518a2
Zuha-SB <71181437+Zuha-SB@users.noreply.github.com>
2023-06-22 15:35:04 -0500

    Added drag and drop functionality

--- chess-app/src/components/Chessboard/Chessboard.tsx
+++ chess-app/src/components/Chessboard/Chessboard.tsx
@@ -1,3 +1,4 @@
+import { act } from '@testing-library/react';
 import Tile from '../Tile/Tile'
 import './Chessboard.css';
 
@@ -34,21 +35,59 @@ for(let i = 0; i < files.length; i++) {
     pieces.push({ piece: "chess-pieces/pawn_w.png", x: i, y: 1 })
 }
 
+let activePiece: HTMLElement | null = null;
+
+function grabPiece(e: React.MouseEvent) {
+    const element = e.target as HTMLElement;
+    if(element.classList.contains("chess-piece")) {
+        const x = e.clientX - 50;
+        const y = e.clientY - 50;
+        element.style.position = "absolute";
+        element.style.left = `${x}px`;
+        element.style.top = `${y}px`;
+
+        activePiece = element;
+    }
+}
+
+function movePiece(e: React.MouseEvent) {
+    if(activePiece) {
+        const x = e.clientX - 50;
+        const y = e.clientY - 50;
+        activePiece.style.position = "absolute";
+        activePiece.style.left = `${x}px`;
+        activePiece.style.top = `${y}px`;
+    }
+}
+
+function dropPiece(e: React.MouseEvent) {
+    if(activePiece) {
+        activePiece = null;
+    }
+}
+
 export default function Chessboard() {
     let board = [];
     for(let j = ranks.length-1; j >= 0; j--) {
         for(let i = 0; i < files.length; i++) {
             const sum = j + i;
             let piece = undefined;
-
             pieces.forEach(p => {
                 if(p.x === i && p.y === j) {
                     piece = p.piece;
                 }
             })
-
             board.push(<Tile key={`${j},${i}`} piece={piece} number={sum}/>);
         }
     }
-    return <div id="chessboard">{board}</div>
+    return (
+        <div 
+            onMouseMove={(e) => movePiece(e)} 
+            onMouseDown={(e) => grabPiece(e)} 
+            onMouseUp={(e) => dropPiece(e)}
+            id="chessboard"
+        >
+            {board}
+        </div>
+    );
 }
