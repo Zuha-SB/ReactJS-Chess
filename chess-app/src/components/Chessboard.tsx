f6db25837cc0de5f14cdec4efa391539a7799f4f
Zuha-SB <71181437+Zuha-SB@users.noreply.github.com>
2023-06-22 11:03:27 -0500

    Created background and chessboard

--- chess-app/src/components/Chessboard.tsx
+++ chess-app/src/components/Chessboard.tsx
@@ -1,6 +1,20 @@
 import React from 'react';
 import './Chessboard.css';
 
+const ranks = ["1", "2", "3", "4", "5", "6", "7", "8"];
+const files = ["a", "b", "c", "d", "e", "f", "g", "h"];
+
 export default function Chessboard() {
-    return <div>Hello, I am the chessboard!</div>
+    let board = [];
+    for(let j = ranks.length-1; j >= 0; j--) {
+        for(let i = 0; i < files.length; i++) {
+            const sum = j + i;
+            if(sum % 2 === 0) {
+                board.push(<div className='tile black-tile'></div>);
+            } else {
+                board.push(<div className='tile white-tile'></div>);
+            }
+        }
+    }
+    return <div id="chessboard">{board}</div>
 }
