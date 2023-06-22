21cb3dcc7276bd9b6ed61c95cdb073f7807448ef
Zuha-SB <71181437+Zuha-SB@users.noreply.github.com>
2023-06-22 12:05:10 -0500

    Split chessboard and tile components

--- chess-app/src/components/Chessboard/Chessboard.tsx
+++ chess-app/src/components/Chessboard/Chessboard.tsx
@@ -1,3 +1,4 @@
+import Tile from '../Tile/Tile'
 import './Chessboard.css';
 
 const ranks = ["1", "2", "3", "4", "5", "6", "7", "8"];
@@ -8,11 +9,7 @@ export default function Chessboard() {
     for(let j = ranks.length-1; j >= 0; j--) {
         for(let i = 0; i < files.length; i++) {
             const sum = j + i;
-            if(sum % 2 === 0) {
-                board.push(<div className='tile black-tile'></div>);
-            } else {
-                board.push(<div className='tile white-tile'></div>);
-            }
+            board.push(<Tile number={sum}/>);
         }
     }
     return <div id="chessboard">{board}</div>
