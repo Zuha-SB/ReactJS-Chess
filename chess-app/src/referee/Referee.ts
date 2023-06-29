6827c5bd5e22282683f3c8c413f82a8e5499c9c2
Zuha-SB <71181437+Zuha-SB@users.noreply.github.com>
2023-06-29 11:56:25 -0500

    Implemented white pawn attacking logic

--- chess-app/src/referee/Referee.ts
+++ chess-app/src/referee/Referee.ts
@@ -2,8 +2,6 @@ import { PieceType, TeamType, Piece } from "../components/Chessboard/Chessboard"
 
 export default class Referee {
     tileIsOccupied(x: number, y: number, boardState: Piece[]): boolean {
-        console.log("Checking if tile is occupied...");
-
         const piece = boardState.find(p => p.x === x && p.y === y);
         if(piece) {
             return true;
@@ -12,23 +10,47 @@ export default class Referee {
         }
     }
 
+    tileIsOccupiedByOpponent(x: number, y: number, boardState: Piece[], team: TeamType): boolean {
+        const piece = boardState.find((p) => p.x === x && p.y === y && p.team !== team);
+
+        if(piece) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
     isValidMove(px: number, py: number, x: number, y: number, type: PieceType, team: TeamType, boardState: Piece[]) {
-        console.log("Referee is checking the move...");
-        console.log(`Previous location: (${px},${py})`);
-        console.log(`Current location: (${x},${y})`);
-        console.log(`Piece type: ${type}`);
-        console.log(`Team: ${team}`);
+        // console.log("Referee is checking the move...");
+        // console.log(`Previous location: (${px},${py})`);
+        // console.log(`Current location: (${x},${y})`);
+        // console.log(`Piece type: ${type}`);
+        // console.log(`Team: ${team}`);
 
         if(type === PieceType.PAWN) {
             const specialRow = (team === TeamType.OUR) ? 1 : 6;
             const pawnDirection = (team === TeamType.OUR) ? 1 : -1;
 
+            //Movement logic
             if(px === x && py === specialRow && y - py === 2 * pawnDirection) {
                 if(!this.tileIsOccupied(x, y, boardState) && !this.tileIsOccupied(x, y - pawnDirection, boardState)) {
                     return true;
                 }
             } else if(px === x && y - py === pawnDirection){
                 if(!this.tileIsOccupied(x, y, boardState)) {
+                    return true;
+                }
+            }
+
+            //Attack logic
+            else if(x - px === -1 && y - py === pawnDirection) {
+                //Attack to the left
+                if(this.tileIsOccupiedByOpponent(x, y, boardState, team)) {
+                    return true;
+                }
+            } else if(x - px === 1 && y - py === pawnDirection) {
+                //Attack to the right
+                if(this.tileIsOccupiedByOpponent(x, y, boardState, team)) {
                     return true;
                 }
             }
