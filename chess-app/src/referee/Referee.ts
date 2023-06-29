a0f88c92854f1a1c366d6e0805706fbec38ae057
Zuha-SB <71181437+Zuha-SB@users.noreply.github.com>
2023-06-29 17:32:12 -0500

    Implemented en passant move

--- chess-app/src/referee/Referee.ts
+++ chess-app/src/referee/Referee.ts
@@ -20,6 +20,20 @@ export default class Referee {
         }
     }
 
+    isEnPassantMove(px: number, py: number, x: number, y: number, type: PieceType,team: TeamType, boardState: Piece[]) {
+        const pawnDirection = team === TeamType.OUR ? 1 : -1;
+
+        if(type === PieceType.PAWN) {
+            if((x - px === -1 || x - px === 1) && y - py === pawnDirection) {
+                const piece = boardState.find((p) => p.x === x && p.y === y - pawnDirection && p.enPassant );
+                if(piece) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
     isValidMove(px: number, py: number, x: number, y: number, type: PieceType, team: TeamType, boardState: Piece[]) {
         // console.log("Referee is checking the move...");
         // console.log(`Previous location: (${px},${py})`);
