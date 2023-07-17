d3104af2461c53a13b6095243868dce3f8516113
Zuha-SB <71181437+Zuha-SB@users.noreply.github.com>
2023-07-17 10:37:19 -0500

    Basic rook movement logic

--- chess-app/src/referee/Referee.ts
+++ chess-app/src/referee/Referee.ts
@@ -148,6 +148,25 @@ export default class Referee {
                     }
                 }
             }
+        } else if(type === PieceType.ROOK) {
+            if(initialPosition.x === desiredPosition.x) {
+                for(let i = 1; i < 8; i++) {
+                    let multiplier = (desiredPosition.y < initialPosition.y) ? -1 : 1;
+                    let passedPosition: Position = {x: initialPosition.x, y: initialPosition.y + (i*multiplier)};
+                    if(passedPosition.x === desiredPosition.x && passedPosition.y === desiredPosition.y) {
+                        return true;
+                    }
+                }
+            }
+            if(initialPosition.y === desiredPosition.y) {
+                for(let i = 1; i < 8; i++) {
+                    let multiplier = (desiredPosition.x < initialPosition.x) ? -1 : 1;
+                    let passedPosition: Position = {x: initialPosition.x + (i*multiplier), y: initialPosition.y};
+                    if(passedPosition.x === desiredPosition.x && passedPosition.y === desiredPosition.y) {
+                        return true;
+                    }
+                }
+            }
         }
         return false;
     }
