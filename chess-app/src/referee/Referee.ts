578716f61f022305f1c9be95788ee3032ff0a839
Zuha-SB <71181437+Zuha-SB@users.noreply.github.com>
2023-07-22 18:26:24 -0500

    Simplified queen logic

--- chess-app/src/referee/Referee.ts
+++ chess-app/src/referee/Referee.ts
@@ -191,48 +191,34 @@ export default class Referee {
     }
     
     queenMove(initialPosition: Position, desiredPosition: Position, team: TeamType, boardState: Piece[]): boolean {
-
         for(let i = 1; i < 8; i++) {
-                        
-            //VERTICAL MOVEMENT
-            if(initialPosition.x === desiredPosition.x) {
-                let multiplier = (desiredPosition.y < initialPosition.y) ? -1 : 1;
-                let passedPosition: Position = {x: initialPosition.x, y: initialPosition.y + (i*multiplier)};
-                if(samePosition(passedPosition, desiredPosition)) {
-                    if(this.tileIsEmptyOrOccupiedByOpponent(passedPosition, boardState, team)) {
-                        return true;
-                    }
-                } else {
-                    if(this.tileIsOccupied(passedPosition, boardState)) {
-                        break;
-                    }
-                }
-            } else if(initialPosition.y === desiredPosition.y) {
-                //HORIZONTAL MOVEMENT
-                let multiplier = (desiredPosition.x < initialPosition.x) ? -1 : 1;
-                let passedPosition: Position = {x: initialPosition.x + (i*multiplier), y: initialPosition.y};
-                if(samePosition(passedPosition, desiredPosition)) {
-                    if(this.tileIsEmptyOrOccupiedByOpponent(passedPosition, boardState, team)) {
-                        return true;
-                    }
-                } else {
-                    if(this.tileIsOccupied(passedPosition, boardState)) {
-                        break;
-                    }
+            let multiplierX;
+            let multiplierY;
+
+            if(desiredPosition.x < initialPosition.x) {
+                multiplierX = -1;
+            } else if(desiredPosition.x > initialPosition.x) {
+                multiplierX = 1;
+            } else {
+                multiplierX = 0;
+            }
+
+            if(desiredPosition.y < initialPosition.y) {
+                multiplierY = -1;
+            } else if(desiredPosition.y > initialPosition.y) {
+                multiplierY = 1;
+            } else {
+                multiplierY = 0;
+            }
+
+            let passedPosition: Position = {x: initialPosition.x + (i * multiplierX), y: initialPosition.y + (i * multiplierY)};
+            if(samePosition(passedPosition, desiredPosition)) {
+                if(this.tileIsEmptyOrOccupiedByOpponent(passedPosition, boardState, team)) {
+                    return true;
                 }
             } else {
-                //DIAGONAL MOVEMENT
-                let multiplierX = (desiredPosition.x < initialPosition.x) ? -1 : 1;
-                let multiplierY = (desiredPosition.y < initialPosition.y) ? -1 : 1;
-                let passedPosition: Position = {x: initialPosition.x + (i * multiplierX), y: initialPosition.y + (i * multiplierY)};
-                if(samePosition(passedPosition, desiredPosition)) {
-                    if(this.tileIsEmptyOrOccupiedByOpponent(passedPosition, boardState, team)) {
-                        return true;
-                    }
-                } else {
-                    if(this.tileIsOccupied(passedPosition, boardState)) {
-                        break;
-                    }
+                if(this.tileIsOccupied(passedPosition, boardState)) {
+                    break;
                 }
             }
         }
