a98f0c19b7fecc2581b593656826cd218eeb58ec
Zuha-SB <71181437+Zuha-SB@users.noreply.github.com>
2023-07-22 22:32:16 -0500

    Adding initial 2 step to valid pawn moves

--- chess-app/src/referee/rules/PawnRules.ts
+++ chess-app/src/referee/rules/PawnRules.ts
@@ -34,10 +34,14 @@ export const pawnMove = (initialPosition: Position, desiredPosition: Position, t
 export const getPossiblePawnMoves = (pawn: Piece, boardState: Piece[]) : Position[] => {
     const possibleMoves: Position[] = [];
 
+    const specialRow = (pawn.team === TeamType.OUR) ? 1 : 6;
     const pawnDirection = (pawn.team === TeamType.OUR) ? 1 : -1;
 
     if(!tileIsOccupied({x: pawn.position.x, y: pawn.position.y + pawnDirection}, boardState)) {
         possibleMoves.push({x: pawn.position.x, y: pawn.position.y + pawnDirection});
+        if(pawn.position.y === specialRow && !tileIsOccupied({x: pawn.position.x, y: pawn.position.y + pawnDirection * 2}, boardState)) {
+            possibleMoves.push({x: pawn.position.x, y: pawn.position.y + pawnDirection * 2});
+        }
     }
 
     return possibleMoves;
