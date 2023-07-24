e033410901df18a8a3dfaacd8c65832b15caaa07
Zuha-SB <71181437+Zuha-SB@users.noreply.github.com>
2023-07-23 22:06:20 -0500

    Fixed valid moves bug by making the dot a circle

--- chess-app/src/components/Tile/Tile.tsx
+++ chess-app/src/components/Tile/Tile.tsx
@@ -11,7 +11,8 @@ export default function Tile({ number, piece, highlight }: Props) {
     const className: string = ["tile", 
         number % 2 === 0 && "black-tile", 
         number % 2 !==0 && "white-tile", 
-        highlight && "tile-highlight"].filter(Boolean).join(' ');
+        highlight && "tile-highlight",
+        piece && "chess-piece-tile"].filter(Boolean).join(' ');
 
     return (
         <div className={className}>
